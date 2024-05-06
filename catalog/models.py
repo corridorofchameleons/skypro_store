ca169f534170e72ad85be9e931225c699bdc45a1
speedfreak <gdramze@gmail.com>
2024-05-06 17:43:14 +0300

    add fixtures

--- catalog/models.py
+++ catalog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Категория')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Категория'
@@ -17,8 +17,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена')
     created_at = models.DateTimeField(verbose_name='Дата создания')
