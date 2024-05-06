cd538bb212b6a53996ec8e83790a83d329cc2fc1
speedfreak <gdramze@gmail.com>
2024-05-06 16:17:32 +0300

    add models

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.apps import CatalogConfig
@@ -9,3 +11,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('contacts/', contacts, name='contacts')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
